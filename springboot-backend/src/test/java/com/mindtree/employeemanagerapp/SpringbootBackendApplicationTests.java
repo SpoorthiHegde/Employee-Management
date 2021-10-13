efeda3a8cbd0dbad37b5381fe295c8f623ea4f93
SpoorthiHegde <spoorthihegde18@gmail.com.com>
2021-10-13 12:56:55 +0530

    changed test1

--- springboot-backend/src/test/java/com/mindtree/employeemanagerapp/SpringbootBackendApplicationTests.java
+++ springboot-backend/src/test/java/com/mindtree/employeemanagerapp/SpringbootBackendApplicationTests.java
@@ -5,11 +5,13 @@ import java.util.Optional;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 
 import com.mindtree.employeemanagerapp.model.Employee;
 import com.mindtree.employeemanagerapp.repository.EmployeeRepository;
@@ -24,6 +26,7 @@ class SpringbootBackendApplicationTests {
 //---------------------Get List of Employees Test ------------------------
 
 	@Test
+	@Order(3)
 	public void getAllEmployees() {
 		List<Employee> employees = employeeRepository.findAll();
 		Assertions.assertThat(employees.size()).isGreaterThan(0);
@@ -31,16 +34,19 @@ class SpringbootBackendApplicationTests {
 
 // ---------------Create Employee Test-------------------------------
 
-//	@Test
-//	public void createEmployeeTest() {
-//		Employee employee = new Employee("Spoorthi", "Hegde", "Spoo@gmail.com");
-//		employeeRepository.save(employee);
-//		Assertions.assertThat(employee.getId()).isGreaterThan(0);
-//	}
+	@Test
+	@Order(1)
+	@Rollback(value = false)
+	public void createEmployeeTest() {
+		Employee employee = new Employee("Spoorthi", "Hegde", "Spoo@gmail.com");
+		employeeRepository.save(employee);
+		Assertions.assertThat(employee.getId()).isGreaterThan(0);
+	}
 
 //---------------------Get Employee by Id Test-----------------------
 
 	@Test
+	@Order(2)
 	public void getEmployeeTest() {
 		Employee employee = employeeRepository.findById(3L).get();
 		Assertions.assertThat(employee.getId()).isEqualTo(3);
@@ -49,6 +55,8 @@ class SpringbootBackendApplicationTests {
 //---------------------Update Employee Test--------------------------
 
 	@Test
+	@Order(4)
+	@Rollback(value = false)
 	public void updateEmployeeTest() {
 		Employee employee = employeeRepository.findById(3L).get();
 		employee.setEmailId("bob@gmail.com");
@@ -58,6 +66,8 @@ class SpringbootBackendApplicationTests {
 
 //----------------------- Delete Employee Test------------------------
 	@Test
+	@Order(5)
+	@Rollback(value = false)
 	public void deleteEmployeeTest() {
 
 		Employee employee = employeeRepository.findById(17L).get();
